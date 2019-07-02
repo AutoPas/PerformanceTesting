7683f4002f992aad249f71aa42938a56f4489ffd
kruegener <git@moritzkruegener.com>
2019-07-03 01:53:39 +0200

    Run Measure Script

--- Commit.py
+++ Commit.py
@@ -7,11 +7,13 @@ from model.Config import Config
 
 # TODO: DISABLE
 FAKE_BUILD = True
+THREADS = 4
 
 class Commit:
 
     baseDir = ""
     buildDir = "build"
+    mdFlexDir = ""
 
     def __init__(self, repo: Repo, commit):
         # Reset Dir to specified commit
@@ -25,15 +27,15 @@ class Commit:
 
     def build(self):
 
-        buildDir = os.path.join(self.baseDir, self.buildDir)
-
-        print("BUILD DIR: ", buildDir)
+        self.buildDir = os.path.join(self.baseDir, self.buildDir)
+        self.mdFlexDir = os.path.join(self.buildDir, "examples/md-flexible")
+        print("BUILD/MD-FLEX DIR: ", self.buildDir, self.mdFlexDir)
 
         # remove old buildDir if present
         if not FAKE_BUILD:
-            shutil.rmtree(buildDir, ignore_errors=True)
-            os.mkdir(buildDir)
-        os.chdir(buildDir)
+            shutil.rmtree(self.buildDir, ignore_errors=True)
+            os.mkdir(self.buildDir)
+        os.chdir(self.buildDir)
 
         # run cmake
         print("Running CMAKE")
@@ -53,8 +55,21 @@ class Commit:
 
 
     def measure(self):
-        # change to build folder
-        os.chdir(self.buildDir)
-        
+        # change to md-flexible folder
+        os.chdir(self.mdFlexDir)
+
+        # main.py path
+        mainPath = os.path.dirname(os.path.abspath(__file__))
+        print(mainPath)
+        # short test script copy to build folder
+        shutil.copy(os.path.join(mainPath, "measurePerf_short.sh"), self.mdFlexDir)
+
+        # export thread number and run test
+        measure_output = run(["./measurePerf_short.sh", "md-flexible"], stdout=PIPE, stderr=PIPE)
+        print(measure_output.stdout, measure_output.stderr)
+
         # change to top
         os.chdir(self.baseDir)
+
+    def upload(self):
+        pass
