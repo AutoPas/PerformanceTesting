8be485f9595b8c51564aa5830877424f97f64a5f
kruegener <moritz.kruegener@gmail.com>
2020-06-10 16:46:27 +0200

    Worker class to run performance measurements away from hooks

---
+++ gitApp/checks/Worker.py
@@ -0,0 +1,62 @@
+import mongoengine as me
+
+from model.QueueObject import QueueObject
+from model.Config import Config
+from hook.CheckFlow import CheckFlow
+
+
+class Worker:
+    """
+    Worker class running all performance measurements currently in queue
+    """
+
+    def __init__(self):
+        self.checkflow = CheckFlow()  # Handles Repo and Auth calls
+
+
+    def checkQueue(self):
+        """
+        Checks Queue for remaining tests recursively
+        :return: True if queue is
+        """
+        nextUp: QueueObject
+        queue = QueueObject.objects(running=False)
+        if len(queue) != 0:
+            nextUp = queue.first()
+            del queue
+        else:
+            return True
+
+        nextUp.running = True
+        nextUp.save()  # Update status to running
+
+        sha = nextUp.commitSHA
+
+        try:
+            self.checkflow.auth.updateInstallID(nextUp.installID)
+            if self.checkflow.runCheck(sha, nextUp.runUrl):  # Run perf measurements
+                if nextUp.compareUrl is not None:
+                    self.checkflow.comparePerformance(sha, nextUp.compareUrl)  # Run comparison
+            nextUp.status = "completed"
+            nextUp.save()
+        except Exception as exc:
+            nextUp.status = str(exc)
+            nextUp.save()
+
+        # Recursion to check for remaining queue
+        if self.checkQueue():
+            print('Queue is done')
+
+
+### RUNNING IN STANDALONE MODE IN POD ###
+if __name__ == '__main__':
+
+    try:
+        w = Worker()
+        w.checkQueue()
+        exit(0)
+    except Exception as e:
+        print('\n\n\n\n WORKER FAILED \n\n\n\n')
+        print(e)
+        print('\n\n\n\n WORKER FAILED \n\n\n\n')
+        exit(-1)
