0e0827c1962a4a1dc35ae5cded1c13a2d60f0317
kruegener <moritz.kruegener@gmail.com>
2020-06-26 00:38:46 +0200

    doc

--- gitApp/checks/yamlSetups/uploadSetups.py
+++ gitApp/checks/yamlSetups/uploadSetups.py
@@ -2,7 +2,15 @@ import mongoengine as me
 from mongoDocuments.Setup import Setup
 
 
+"""
+Script used to upload all .yaml files in this directory, and if changes occur for same name files, update version number
+
+"""
 if __name__ == '__main__':
 
     # Connect to DB
     me.connect(url='localhost:30017', user='XXX', password='XXX')
+
+    # TODO: check for same file content
+
+    # TOOD: if not same, but same name, update version number, deactivate last version
