70a59658d3c7f7156bdd6824e2986ade7db86132
kruegener <moritz.kruegener@gmail.com>
2020-06-26 01:32:27 +0200

    change hashing mechanism and enforce only one active setup per name

--- gitApp/checks/yamlSetups/uploadSetups.py
+++ gitApp/checks/yamlSetups/uploadSetups.py
@@ -1,5 +1,9 @@
-import mongoengine as me
 from mongoDocuments.Setup import Setup
+
+import mongoengine as me
+import glob
+import hashlib
+from datetime import datetime
 
 
 """
@@ -12,5 +16,28 @@ if __name__ == '__main__':
     me.connect(url='localhost:30017', user='XXX', password='XXX')
 
     # TODO: check for same file content
+    for filename in glob.glob('*.yaml'):
+        print(filename)
+        s = Setup()
+        s.name = filename
+        s.uploadDate = datetime.utcnow()
 
-    # TOOD: if not same, but same name, update version number, deactivate last version
+        with open(filename, 'r') as f:
+            file = f.read()
+            s.yaml = file
+            s.yamlHash = hashlib.md5(file.encode('utf-8')).hexdigest()
+
+        # Checking File uniqueness via yaml hash for a given name
+        try:
+            s.active = True
+            s.save()
+            print('New Setup saved\n')
+
+            # Deactivating old versions
+            for old in Setup.objects(name=s.name, yamlHash__ne=s.yamlHash):
+                old.active = False
+                old.save()
+
+        except me.NotUniqueError:
+            print('File already present\n')
+            continue
--- gitApp/mongoDocuments/Setup.py
+++ gitApp/mongoDocuments/Setup.py
@@ -6,11 +6,11 @@ class Setup(me.DynamicDocument):
     Holding .yaml file used for performance testing runs
     """
 
-    # Yaml File
-    name = me.StringField(unique=False)
-    version = me.IntField(unique=True)
-    file = me.FileField(unique=True)
-    active = me.BooleanField()
+    name = me.StringField(unique=False)  # Name of setup, can be kept and updated with changed file
+    yaml = me.StringField(unique=False)  # Yaml String
+    yamlHash = me.StringField(unique=True)  # MD5 hash to check for file uniqueness given a setup name
+    active = me.BooleanField()  # Only one setup with the same name shall be active
+    uploadDate = me.DateTimeField()
 
     def __str__(self):
         out = f'{self.name}'
