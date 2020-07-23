f92d05092ec83ef03aab2501f271a0cee1be01ca
kruegener <moritz.kruegener@gmail.com>
2020-07-23 19:02:53 +0200

    read the docs theme

--- docs/conf.py
+++ docs/conf.py
@@ -34,9 +34,9 @@ release = 'alpha'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['recommonmark', 'sphinx.ext.autodoc', 'sphinx.ext.napoleon']
+extensions = ['recommonmark', 'sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx_rtd_theme']
 autodoc_default_options = {"members": True, "undoc-members": True, "private-members": True}
-autodoc_mock_imports = []  # ["mongoengine"]
+autodoc_mock_imports = ["mongoengine"]
 autodoc_inherit_docstrings = True
 autodoc_member_order = 'alphabetical'  # 'bysource'
 
@@ -54,7 +54,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
