087c1d5461f801f280aad153a8ca8f67c68ad174
kruegener <moritz.kruegener@gmail.com>
2020-07-25 00:17:13 +0200

    paths v3

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,9 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../PerformanceTesting/gitApp'))
+sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('../gitApp'))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gitApp.settings')
 
 # -- Project information -----------------------------------------------------
