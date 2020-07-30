d8b8974293bd1ff44f7f571e1fffd95938f66f00
kruegener <moritz.kruegener@gmail.com>
2020-07-30 21:24:44 +0200

    change checks import order

--- gitApp/checks/CheckFlow.py
+++ gitApp/checks/CheckFlow.py
@@ -15,11 +15,11 @@ except ModuleNotFoundError:
     BASE_DIR = os.path.join('../gitApp', BASE_DIR)
 from hook.helper import pretty_request, initialStatus, codeStatus, spawnWorker, get_dyn_keys, generate_label_table
 from checks import Authenticator
-from checks import Repository
-from mongoDocuments.Config import Config
-from mongoDocuments.QueueObject import QueueObject
-from mongoDocuments.Result import Result
 from checks import ImgurUploader
+from checks import Repository
+from mongoDocuments import Config
+from mongoDocuments import QueueObject
+from mongoDocuments import Result
 
 """
 Codes:
