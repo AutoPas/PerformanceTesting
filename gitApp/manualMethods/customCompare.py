f66a9708fc4c05ec166e4873dca4db3a63b38195
kruegener <moritz.kruegener@gmail.com>
2020-08-06 14:50:43 +0200

    time stamping custom compares

--- gitApp/manualMethods/customCompare.py
+++ gitApp/manualMethods/customCompare.py
@@ -1,15 +1,17 @@
 from mongoDocuments import QueueObject
 from checks import CheckFlow
+import time
+
 
 if __name__ == '__main__':
 
-    testSHA = "3114972f9816c81a5c8f74a13495c287861793b0"
+    testSHA = "1baed181eaf3e698b8e7061a8ac8d0607844d39f"
     compareSHA = 'a3193c3dfc47afd976b1e1061bddb579b04c7ab9'
 
     c = CheckFlow(initRepo=False)
     c.baseUrl = "https://api.github.com/repos/AutoPas/AutoPas"
     c.auth.updateInstallID(2027548)
-    compareUrl = c._createCheckRun(testSHA, "MANUAL COMPARE")
+    compareUrl = c._createCheckRun(testSHA, f"MANUAL COMPARE {time.asctime()}")
 
     q = QueueObject()
     q.compareUrl = compareUrl
