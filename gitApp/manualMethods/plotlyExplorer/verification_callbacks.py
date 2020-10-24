fc7ba6372cfd40a6d80f2ec21366174d76c945cc
kruegener <moritz.kruegener@gmail.com>
2020-10-24 23:02:16 +0200

    remove unused imports

--- gitApp/manualMethods/plotlyExplorer/verification_callbacks.py
+++ gitApp/manualMethods/plotlyExplorer/verification_callbacks.py
@@ -5,8 +5,6 @@ import requests
 import re
 import os
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
 from dash.dependencies import Input, Output, State, ALL
 
 
